b9f72850e6005258467b495b28cdd74d367dbbba
constance.de-quatrebarbes <constance.de-quatrebarbes@developpement-durable.gouv.fr>
2022-05-22 22:29:39 +0200

    TEST import

--- backend/test_rule.py
+++ backend/test_rule.py
@@ -1,27 +1,29 @@
+from csv import DictReader
+import datetime
 from .rule import Rule
 # from .rule import CSVItem
+
 def cast_type_from_str_to_python(element):
-    if element in ["True", "False"]:
+    if element in ["True", "False", 1, 0, "true", "false"]:
         return bool(element)
     if element.isnumeric():
         return int(element)
-    # elif "-" in element or ":" in  element:
+    if "-" in element and ":" in element:
+        return datetime.datetime.strptime(element, "%Y-%M-%d %H:%m:%s")
+    if "-" in element:
+        return datetime.datetime.strptime(element, "%Y-%M-%d")
     return element
 
 def test_rule_001():
     """
     test init from a fake oneliner csv
     """
-    header = "field,model,name_fr,name_en,section_order,section_name_fr,section_name_en,external_model_name,external_model_display_keys,is_controled,reference_table,vocab,inspire,translation,multiple,constraint,datatype,format,search,filter,admin,list,item,required,description_fr,description_en,example_fr,example_en,default_fr,comment,default_en"
-    value = "environment_detail,dataset,Milieu,,4,Santé Environnement,Health and Environnement,reference,name|id,True,ref_environment_detail,dcat:themeTaxonomy: skos,,True,True,one of,string,,True,False,18,18,18,True,Ce champ permet de définir le milieu concerné par la ressource à partir d'un vocabulaire contrôlé interne,,N/D,N/D,N/D,champ par défault en cours de construction,"
+    header = "field,model,name_fr,name_en,section_order,section_name_fr,section_name_en,external_model_name,external_model_display_keys,is_controled,reference_table,vocab,inspire,translation,multiple,constraint,datatype,format,search,filter,admin,list,item,required,description_fr,description_en,example_fr,example_en,default_fr,comment,default_en, issue_date"
+    value = "environment_detail,dataset,Milieu,Environment,4,Santé Environnement,Health and Environment,reference,name|id,True,ref_environment_detail,dcat:themeTaxonomy: skos,,True,True,one of,string,,True,False,18,18,18,True,Ce champ permet de définir le milieu concerné par la ressource à partir d'un vocabulaire contrôlé interne,,N/D,N/D,N/D,champ par défault en cours de construction,,2022/05/22"
     matrix = []
     for element in value.split(","):
-        if "|" in element:
-            matrix.append([cast_type_from_str_to_python(e) for e in element.split("|")])
-        else:
-            matrix.append(cast_type_from_str_to_python(element))
+        matrix.append(cast_type_from_str_to_python(element))
     rule_d = dict(zip(header.split(","),matrix))
-    print(rule_d)
     r = Rule(**rule_d)
     assert r.is_controled
     assert r.multiple
@@ -29,4 +31,32 @@ def test_rule_001():
     assert r.filter
     assert r.search
     assert r.external_model_name == "reference"
+    assert r.external_model_display_keys == ["name", "id"]
     assert r.admin != -1
+def test_rule_export_002():
+    """test rules template generarion in csv"""
+    header = "field,model,name_fr,name_en,section_order,section_name_fr,section_name_en,external_model_name,external_model_display_keys,is_controled,reference_table,vocab,inspire,translation,multiple,constraint,datatype,format,search,filter,admin,list,item,required,description_fr,description_en,example_fr,example_en,default_fr,comment,default_en, issue_date"
+    value = "environment_detail,dataset,Milieu,Environment,4,Santé Environnement,Health and Environment,reference,name|id,True,ref_environment_detail,dcat:themeTaxonomy: skos,,True,True,one of,string,,True,False,18,18,18,True,Ce champ permet de définir le milieu concerné par la ressource à partir d'un vocabulaire contrôlé interne,,N/D,N/D,N/D,champ par défault en cours de construction,,2022/05/22"
+    matrix = []
+    for element in value.split(","):
+        # if "|" in element:
+        #     matrix.append([cast_type_from_str_to_python(e) for e in element.split("|")])
+        # else:
+        matrix.append(cast_type_from_str_to_python(element))
+    rule_d = dict(zip(header.split(","),matrix))
+    r = Rule(**rule_d)
+    print(r._export())
+    assert r._export() == "\n".join([header, value]), r._export()
+def test_rule_setter_003():
+    """test with csv file"""
+    with open("test_rules.csv", "r") as f:
+        reader = DictReader(f, delimiter=",")
+        for row in reader:
+            rule_d = {}
+            for k, v in row.items():
+                if "|" in v:
+                    v = [cast_type_from_str_to_python(e) for e in v.split("|")]
+                    rule_d[k] = v
+                else:
+                    v = cast_type_from_str_to_python(v) 
+                    rule_d[k] = v
